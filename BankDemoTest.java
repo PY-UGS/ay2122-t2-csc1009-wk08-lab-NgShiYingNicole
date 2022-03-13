fefc411e19d9f740eed794fbeea0fa47f31e5266
NgShiYingNicole <97488614+NgShiYingNicole@users.noreply.github.com>
2022-03-14 03:06:13 +0800

    Update BankDemoTest.java
    change syntax

--- BankDemoTest.java
+++ BankDemoTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BankDemoTest{
 	public static void main(String[] args) {
 		try
-        {
+		{
 			CheckingAccount account = new CheckingAccount("ABC1234");
 			Scanner input = new Scanner(System.in);
 			//a. Deposit $xx into the account object using keyboard input. 
@@ -17,11 +17,11 @@ public class BankDemoTest{
 			account.withdraw(withdrawAmount);
 			//c. If $yy < $xx, print out the account balance “The balance after withdraw is: $ ”. 
 			System.out.println("The balance after withdraw is: $ "+ account.getBalance());
-        }
-        catch ( Exception e)
-        {
-        	//d. If $yy > $xx, print the user-defined exception and short amount “Sorry, but your account is short by: $ ”.
-            System.out.println("Sorry, but your account is short by: $ "+e.getMessage());
-        }
+		}
+		catch ( Exception e)
+		{
+			//d. If $yy > $xx, print the user-defined exception and short amount “Sorry, but your account is short by: $ ”.
+			System.out.println("Sorry, but your account is short by: $ "+e.getMessage());
+		}
 	}
 }
